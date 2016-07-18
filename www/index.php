c843107e586a7a6d382ec2232218a042caa03377
Sgt. Kabukiman <sgt@kabukiman.org>
2016-07-18 22:34:41 +0200

    add primitive maintenance switch for easier deployments

--- www/index.php
+++ www/index.php
@@ -9,6 +9,21 @@
  */
 
 define('HORARO_ROOT', dirname(__DIR__));
+
+$file = HORARO_ROOT.'/maintenance';
+
+if (file_exists($file)) {
+	$allowedIPs = array_map('trim', file($file));
+
+	if (isset($_SERVER['REMOTE_ADDR']) && !in_array($_SERVER['REMOTE_ADDR'], $allowedIPs)) {
+		header('HTTP/1.1 503 Service Unavailable');
+		header('Cache-Control: private, no-cache');
+		header('Expires: Tue, 09 Apr 1975 12:00:00 GMT');
+		print file_get_contents(HORARO_ROOT.'/resources/maintenance.html');
+		exit(1);
+	}
+}
+
 require HORARO_ROOT.'/vendor/autoload.php';
 
 Symfony\Component\HttpFoundation\Request::enableHttpMethodParameterOverride();
