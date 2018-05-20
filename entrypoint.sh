f09c6070ffce91e71d35c32b447c76a746a621bb
Sgt. Kabukiman <sgt@kabukiman.org>
2018-05-20 11:21:45 +0200

    ensure better permissions for some directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 # generate Doctrine proxies
 su horaro -c 'php vendor/doctrine/orm/bin/doctrine orm:generate-proxies'
 
+# make sure mounted directories have proper permissions
+chown horaro:horaro tmp/upload tmp/session log
+
 # hand control over to supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
